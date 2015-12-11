4086466e814b3834fa9bbf77913e8a6adc476945
zhangchengfu <compowerxp@163.com>
2015-12-11 22:31:14 +0800

    更改配置文件，前台能够访问后台

---
+++ src/main/java/com/laozhang/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.laozhang.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.laozhang.domain.User;
+import com.laozhang.service.IUserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private IUserService service;
+	
+	@RequestMapping(value = "/showUser", method = RequestMethod.GET)
+	public ModelAndView getUser(@RequestParam int id) {
+		ModelAndView modeAndView = new ModelAndView();
+		User user = service.getUserById(id);
+		modeAndView.addObject("user", user);
+		modeAndView.setViewName("/showUser");
+		//System.out.println("userName:" + user.getUserName());
+		return modeAndView;
+	}
+}
