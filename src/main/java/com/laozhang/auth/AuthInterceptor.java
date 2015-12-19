cd2edde498c56b73244ada2888965e60373434f9
zhangchengfu <compowerxp@163.com>
2015-12-19 16:18:45 +0800

    拦截器

--- src/main/java/com/laozhang/auth/AuthInterceptor.java
+++ src/main/java/com/laozhang/auth/AuthInterceptor.java
@@ -14,6 +14,7 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
+		System.out.println("++++++++++++++++++++++interceptor++++++++++++++++++++++++++++++++");
 		if(handler.getClass().isAssignableFrom(HandlerMethod.class)){
 			Auth auth = ((HandlerMethod)handler).getMethod().getDeclaringClass().getAnnotation(Auth.class);
 			//没有声明需要权限,或者声明不验证权限
@@ -21,10 +22,10 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 				return true;
 			} else {
 				//todo something
+				return true;
 			}
 		} else {
 			return true;
 		}
-		return super.preHandle(request, response, handler);
 	}
 }
